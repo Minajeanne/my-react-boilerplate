bfdeaf237b3c3939d179c21c6cc4cf4097abc072
Minajeanne <albrown07@mac.com>
2019-11-25 16:36:35 -0500

    installed style loaders & added to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,16 @@ module.exports = {
         test: /\.js$/,
         exclude: ['node_modules'],
         use: [{ loader: 'babel-loader' }],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        user: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
+        }],
       }
     ]
   }
