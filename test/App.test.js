2f2ec8aeaa17222dd59a79b88a95780e2ea0fe53
Minajeanne <albrown07@mac.com>
2019-11-25 16:16:58 -0500

    fix typos

--- test/App.test.js
+++ test/App.test.js
@@ -6,7 +6,7 @@ describe('App', () => {
   test('should match snapshot', () => {
     const wrapper = shallow(<App />)
 
-    expect(wrapper.find(h1).text()).toBe('Welcome to My Starter App')
+    expect(wrapper.find('h1').text()).toBe('Welcome to My Starter App')
     expect(wrapper).toMatchSnapshot
   });
 });
--- test/enzyme.setup.js
+++ test/enzyme.setup.js
@@ -1,4 +1,4 @@
-import Emzyme from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({
